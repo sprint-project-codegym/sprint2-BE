2e18840ba0be472d043a6f85f45333e8fd854636
vanphuoc69 <vanphuoc5112@gmail.com>
2022-05-11 20:26:39 +0700

    PhuocMV push BE

--- BE/src/main/java/com/codegym/cinema/repository/TicketRepository.java
+++ BE/src/main/java/com/codegym/cinema/repository/TicketRepository.java
@@ -1,7 +1,46 @@
 package com.codegym.cinema.repository;
 
 import com.codegym.cinema.entity.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket,Integer> {
+    @Query(value = "SELECT * " +
+            "FROM ticket WHERE ticket.ticket_status_id = 1 GROUP BY ticket_id limit ?1, 5", nativeQuery = true)
+    List<Ticket> findAllByBookedTicket(int index);
+
+    @Query(value = "SELECT * " +
+            "FROM ticket WHERE ticket.ticket_status_id = 1 GROUP BY ticket_id", nativeQuery = true)
+    List<Ticket> findAllByBookedTicketNoPage();
+
+    @Query(value = "SELECT * " +
+            "FROM ticket WHERE ticket_status_id = 1 AND ticket_id = ?1", nativeQuery = true)
+    Page<Ticket> searchByTicketId(Integer ticketId, Pageable pageable);
+
+    @Query(value = "SELECT * " +
+            "FROM ticket WHERE ticket_status_id = 1 AND user_id = ?1", nativeQuery = true)
+    Page<Ticket> searchByUserId(Integer userId, Pageable pageable);
+
+    @Query(value = "SELECT * " +
+            "FROM ticket " +
+            "INNER JOIN `user` ON `user`.user_id = ticket.user_id " +
+            "WHERE ticket_status_id = 1 AND `user`.id_card LIKE concat('%',?1,'%')", nativeQuery = true)
+    Page<Ticket> searchByIdCard(String idCard, Pageable pageable);
+
+    @Query(value = "SELECT * " +
+            "FROM ticket " +
+            "INNER JOIN `user` ON `user`.user_id = ticket.user_id " +
+            "WHERE ticket_status_id = 1 AND `user`.phone LIKE concat('%',?1,'%')", nativeQuery = true)
+    Page<Ticket> searchByPhone(String phone, Pageable pageable);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "UPDATE ticket " +
+            "SET ticket.ticket_status_id = 2 " +
+            "WHERE ticket.ticket_id = ?1", nativeQuery = true)
+    void receiveBookedTicket(Integer ticketId);
 }
--- BE/src/main/java/com/codegym/cinema/service/impl/TicketServiceImpl.java
+++ BE/src/main/java/com/codegym/cinema/service/impl/TicketServiceImpl.java
@@ -1,8 +1,57 @@
 package com.codegym.cinema.service.impl;
 
+import com.codegym.cinema.entity.Ticket;
+import com.codegym.cinema.repository.TicketRepository;
 import com.codegym.cinema.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketServiceImpl implements TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    public List<Ticket> findAllByBookedTicket(int index) {
+        return ticketRepository.findAllByBookedTicket(index);
+    }
+
+    @Override
+    public List<Ticket> findAllByBookedTicketNoPage() {
+        return ticketRepository.findAllByBookedTicketNoPage();
+    }
+
+    @Override
+    public Ticket findById(Integer ticketId) {
+        return ticketRepository.findById(ticketId).orElse(null);
+    }
+
+    @Override
+    public Page<Ticket> searchByTicketId(Integer ticketId, Pageable pageable) {
+        return ticketRepository.searchByTicketId(ticketId, pageable);
+    }
+
+    @Override
+    public Page<Ticket> searchByUserId(Integer userId, Pageable pageable) {
+        return ticketRepository.searchByUserId(userId, pageable);
+    }
+
+    @Override
+    public Page<Ticket> searchByIdCard(String idCard, Pageable pageable) {
+        return ticketRepository.searchByIdCard(idCard, pageable);
+    }
+
+    @Override
+    public Page<Ticket> searchByPhone(String phone, Pageable pageable) {
+        return ticketRepository.searchByPhone(phone, pageable);
+    }
+
+    @Override
+    public void receiveBookedTicket(Integer ticketId) {
+        ticketRepository.receiveBookedTicket(ticketId);
+    }
 }
